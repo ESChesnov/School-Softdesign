7fff509e466cbc4ba01a6f78a0100449cd962c24
ESChesnov <chesnovftw@gmail.com>
2016-01-24 22:15:19 +0300

    Реализован жизненный цикл активити

--- app/src/main/java/com/softdesign/school/MainActivity.java
+++ app/src/main/java/com/softdesign/school/MainActivity.java
@@ -3,11 +3,51 @@ package com.softdesign.school;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.softdesign.school.utils.Lg;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lg.e(this.getLocalClassName(), "=======================================");
+        Lg.e(this.getLocalClassName(), "on create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Lg.e(this.getLocalClassName(), "on start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Lg.e(this.getLocalClassName(), "on resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Lg.e(this.getLocalClassName(), "on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Lg.e(this.getLocalClassName(), "on stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Lg.e(this.getLocalClassName(), "on restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Lg.e(this.getLocalClassName(), "on destroy");
     }
 }
